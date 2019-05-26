31175b8e9398475f64859c64c4a8ffcac7dd6479
zobiejrz <12979847+zobiejrz@users.noreply.github.com>
2019-05-25 18:33:28 -0600

    HelloWorld Command Module
    Cooley imports command modules properly and runs using the hyphen prefix

--- Program.cs
+++ Program.cs
@@ -13,18 +13,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+
 namespace dnd_character_storage
 {
     class Cooley
     {
         static void Main(string[] args) => new Cooley().Start().GetAwaiter().GetResult();
         private DiscordSocketClient _client;
+        private CommandService _commands;
         public async Task Start()
         {
             _client = new DiscordSocketClient(new DiscordSocketConfig { 
                 WebSocketProvider = Discord.Net.Providers.WS4Net.WS4NetProvider.Instance
             });
-            /* Begin Commands */
+
+            _commands = new CommandService(new CommandServiceConfig{
+                CaseSensitiveCommands = true,
+                
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
 
             _client.Connected += async () => {
                 Console.WriteLine("SYS - Connected to Discord Gateway as " + _client.CurrentUser);
@@ -32,32 +41,42 @@ namespace dnd_character_storage
             };
 
             _client.Disconnected += async (e) => {
-                Console.WriteLine("ERR - " + e);
-                Console.WriteLine("ERR - Attempting to rejoin");
+                Console.WriteLine("ERR - Disconnected from Discord Gateway: " + e.Message);
                 await Task.Delay(-1);
             };
 
-            _client.JoinedGuild += async (g) => {
-                Console.WriteLine("SYS - Joined " + g.Name);
-                await g.DefaultChannel.SendMessageAsync("Hello everyone! Type '-help' to see a list of commands!");
-                await Task.Delay(-1);
+            _client.MessageReceived += async (SocketMessage a) => {
+                var Message = a as SocketUserMessage;
+                var Context = new SocketCommandContext(_client, Message);
+
+                if (Message.Content == null || Message.Content == "") return;
+                if (Message.Author.IsBot) return;
+
+                int argPos = 0;
+
+                if (!(Message.HasStringPrefix("-", ref argPos) || Message.HasMentionPrefix(_client.CurrentUser, ref argPos))) return;
+
+                var Result = await _commands.ExecuteAsync(Context, argPos, null, MultiMatchHandling.Best);
+
+                if (!Result.IsSuccess) {
+                    Console.WriteLine("ERR - Command `" + Context.Message + "` could not be completed: " + Result.ErrorReason);
+                }
             };
 
-            _client.GuildAvailable += async (g) => {
-                Console.WriteLine("SYS - Connected to " + g.Name);
-                await Task.Delay(-1);
-            };
 
-            _client.LeftGuild += async (g) => {
-                Console.WriteLine("SYS - Left " + g.Name);
-                await Task.Delay(-1);
-            };
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
 
-            /* End Commands */
             var token = "-x-x-x";
-            await _client.LoginAsync(TokenType.Bot, token, true);
-            await _client.StartAsync();
-            await Task.Delay(-1);
+
+            try
+            {
+                await _client.LoginAsync(TokenType.Bot, token, true);
+                await _client.StartAsync();
+                await Task.Delay(-1);
+            } catch (Exception e) {
+                Console.WriteLine("ERR - Couldn't log in: " + e.Message );
+            }
+            
         }
         public void Log(object sender, LogMessage e)
         {
