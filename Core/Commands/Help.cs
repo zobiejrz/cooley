d5fc74facbef25133e6d66faca43d3fc9830a55d
zobiejrz <ben@zobie.com>
2019-06-19 20:54:05 -0400

    Added -help

---
+++ Core/Commands/Help.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+namespace dnd_character_storage.Core.Commands
+{
+    public class Help : Command
+    {
+        [Command("help"), Alias("h", "?")]
+        public async Task help()
+        {
+            await ReplyAsync( $"{Context.User.Mention} here are the commands!"
+                            + "\n-player"
+                            + "\n-class"
+                            + "\n-race"
+                            + "\n-Health"
+                            + "\n-name"
+                            + "\n-alignment"
+                            + "\n-check" );// This will become -<ability>
+            /*
+            **
+            ** -<ability> [ check | set ] <number>
+            **
+            */
+        }
+    }
+
+}
